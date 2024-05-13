919770a29b2deae9f505a515462445e2f7d91fe9
Rodion Lezhnyuk <rodion.lezhnyuk@tealium.com>
2024-05-13 15:20:47 +0200

    temp fix for the project

--- src/main/java/io/quarkus/runtime/FixedClassPathSystemPropertyRecorder.java
+++ src/main/java/io/quarkus/runtime/FixedClassPathSystemPropertyRecorder.java
@@ -3,9 +3,7 @@ package io.quarkus.runtime;
 import java.util.List;
 
 import io.quarkus.runtime.annotations.Recorder;
-import jakarta.enterprise.inject.Alternative;
 
-@Alternative
 @Recorder
 public class FixedClassPathSystemPropertyRecorder {
 	public void set(List<String> allJarPaths) {
